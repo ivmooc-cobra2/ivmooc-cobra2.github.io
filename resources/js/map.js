ba80e1c3ad792bb86a8859acecce618a5b95776b
Tim Thompson <timathom@gmail.com>
2016-04-20 00:55:30 -0400

    Add work in progress.

--- resources/js/map.js
+++ resources/js/map.js
@@ -28,7 +28,7 @@
         var feature = new OpenLayers.Feature.Vector(
                 new OpenLayers.Geometry.Point( lon, lat ).transform(epsg4326, projectTo),
                 {description: "marker number " + i} ,
-                {externalGraphic: '/static/resources/img/location.red.svg', graphicHeight: 24, graphicXOffset:-12, graphicYOffset:-25  }
+                {externalGraphic: '/resources/img/location.red.svg', graphicHeight: 24, graphicXOffset:-12, graphicYOffset:-25  }
             );             
         vectorLayer.addFeatures(feature);
     }                        
